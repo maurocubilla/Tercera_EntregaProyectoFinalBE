7da3d24483d070b05c2dee921b7a0985a88f9246
Mauro Cubilla <mauroscubilla@gmail.com>
2023-10-01 20:18:20 -0300

    tercera entrega

---
+++ src/middlewares/auth.js
@@ -0,0 +1,20 @@
+export const checkRole = (roles)=>{ // roles = ["admin", "superadmin"]
+    return (req,res,next)=>{
+
+        
+        // console.log("req", req.user.role);
+        if(roles.includes(req.user.role)){
+            next();
+        } else {
+            res.json({status:"error", message:"No tienes permisos para usar estos recursos"});
+        }
+    }
+};
+
+export const checkAuthenticated = (req,res,next)=>{
+    if(req.user){
+        next();
+    } else {
+        res.json({status:"error", message:"Debes estar autenticado"});
+    }
+};
